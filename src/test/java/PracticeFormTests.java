639eadb8eceed49376d3f69d70bea2a3f6f30ce7
Rinat Siraev <rsiraev@sberhealth.ru>
2022-09-26 19:41:06 +0500

    Конечный результат

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,28 +1,71 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
     @BeforeAll
-    static void setUp() {
+    static void configuration() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1700x800";
         Configuration.holdBrowserOpen = true;
     }
 
+
     @Test
-    void fillFormTest() {
-        open("/automation-practice-form");
-        $("[id=firstName]").setValue("Elena");
-        $("[id=lastName]").setValue("Siraeva");
-        $("[id=userEmail]").setValue("lena@siraeva.com");
-        $("[id=userNumber]").setValue("9228881554");
-       // $("[id=dateOfBirthInput]").setValue("19 Sep 2022");
-        $("[id=subjectsInput]").setValue("Math");
-        $("[id=currentAddress]").setValue("SSR");
+    void fillFormTest() throws IOException, URISyntaxException {
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue("Elena");
+        $("#lastName").setValue("Siraeva");
+        $("#userEmail").setValue("test@test.com");
+
+        $(byText("Female")).click();
+
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $("[aria-label='Choose Thursday, November 29th, 1990']").click();
+
+        $("#subjectsInput").setValue("Maths").pressEnter();
+
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+
+
+        $("#uploadPicture").uploadFile(download("https://demoqa.com/images/Toolsqa.jpg"));
+
+        $("#currentAddress").setValue("bla bla bla");
+
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
+
+        $("#submit").pressEnter();
+
+        $(".modal-header").shouldHave(Condition.text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(Condition.text("Elena Siraeva"),
+                Condition.text("test@test.com"),
+                Condition.text("Female"),
+                Condition.text("1234567890"),
+                Condition.text("29 November,1990"),
+                Condition.text("Maths"),
+                Condition.text("Sports Reading Music"),
+                Condition.text("Toolsqa.jpg"),
+                Condition.text("bla bla bla"),
+                Condition.text("Haryana Karnal"));
+        $("#closeLargeModal").click();
 
     }
 }
